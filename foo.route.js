e6dacd35213229dff40284df2e5fbbdfd48f85fe
gmvarsha <114397232+gmvarsha@users.noreply.github.com>
2022-12-09 14:54:26 +0530

    Delete foo.route.js

--- foo.route.js
+++
@@ -1,9 +0,0 @@
-
-const{ Router }=require("express");
-const router=new Router();
-
-const fooService=require("../service/foo.service");
-
-router.get("/",fooService.getEmployees);
-router.get("/:id",fooService.getEmployeeById);
-module.exports=router;
